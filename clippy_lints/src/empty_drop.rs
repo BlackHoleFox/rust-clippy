c9f4affefa68694d70b28153d542993f87b301ad
Gryffon Bellish <owenbellish@gmail.com>
2022-03-21 16:15:51 -0400

    fix formatting

--- clippy_lints/src/empty_drop.rs
+++ clippy_lints/src/empty_drop.rs
@@ -1,7 +1,7 @@
 use clippy_utils::diagnostics::span_lint_and_help;
 // use clippy_utils::ty::is_type_lang_item;
 use if_chain::if_chain;
-use rustc_hir::*;
+use rustc_hir::{Body, ExprKind, Impl, ImplItemKind, Item, ItemKind, Node};
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::{declare_lint_pass, declare_tool_lint};
 
@@ -43,9 +43,9 @@ impl LateLintPass<'_> for EmptyDrop {
             if let impl_item_hir = child.id.hir_id();
             if let Some(Node::ImplItem(impl_item)) = cx.tcx.hir().find(impl_item_hir);
             if let ImplItemKind::Fn(_, b) = &impl_item.kind;
-            if let Body {value: func_expr, ..} = cx.tcx.hir().body(*b);
-            if let ExprKind::Block(ref block, _) = func_expr.kind;
-            if block.stmts.is_empty();
+            if let Body { value: func_expr, .. } = cx.tcx.hir().body(*b);
+            if let ExprKind::Block(block, _) = func_expr.kind;
+            if block.stmts.is_empty() && block.expr.is_none();
             then {
                 span_lint_and_help(cx, EMPTY_DROP, item.span, "empty drop implementation", None, "try removing this impl");
             }
